6e3c49d9ccb9c6ff888c01ac377a0ed386e6b11b
wonjun16 <jun9816@naver.com>
2023-11-07 03:32:17 +0900

    맵 제한

--- test.js
+++ test.js
@@ -54,9 +54,9 @@ window.onload = function init()
 	//카메라 시작 지점
 	const camera = new THREE.PerspectiveCamera(75,canvas.width / canvas.height,0.1, 1000);
 	//camera.rotation.y = 45/180*Math.PI;
-	camera.position.x = 5;
+	camera.position.x = -1;
 	camera.position.y = 5;
-	camera.position.z = 5;
+	camera.position.z = 8;
 
 	//const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -76,6 +76,16 @@ window.onload = function init()
 	loader.load('models/billiards_room/scene.gltf', function(gltf){
 	  const map = gltf.scene.children[0];
 	  map.scale.set(1.0,1.0,1.0);
+
+	  // 회전할 각도를 라디안으로 계산
+	  const angleInRadians = 16 * Math.PI / 180;
+
+	  //Euler 객체를 생성하여 회전 각도를 설정
+	  const rotation = new THREE.Euler(0, angleInRadians, 0);
+
+	  // gltf.scene의 rotation 속성에 새로운 회전값을 할당
+	  gltf.scene.rotation.copy(rotation);
+
 	  scene.add(gltf.scene);
 	  animate();
 	}, undefined, function (error) {
@@ -93,26 +103,39 @@ window.onload = function init()
 		const newX = cameraDirection.x * cosAngle - cameraDirection.z * Math.sin(angle);
 		const newZ = cameraDirection.x * Math.sin(angle) + cameraDirection.z * cosAngle;
 
+		let moveX = 0.0;
+		let moveZ = 0.0;
+
 		// 'a'를 누르고 있을 때 왼쪽으로 이동
 		if (APressed) {
-			camera.position.x += newX * 0.1;
-			camera.position.z += newZ * 0.1;
+			moveX += newX * 0.1;
+			moveZ += newZ * 0.1;
 		}
 		// 'd'를 누르고 있을 때 오른쪽으로 이동
 		if (DPressed) {
-			camera.position.x -= newX * 0.1;
-			camera.position.z -= newZ * 0.1;
+			moveX -= newX * 0.1;
+			moveZ -= newZ * 0.1;
 		}
 		// 'w'를 누르고 있을 때 앞으로 이동
 		if (WPressed) {
-			camera.position.x += cameraDirection.x * 0.1;
-			camera.position.z += cameraDirection.z * 0.1;
+			moveX += cameraDirection.x * 0.1;
+			moveZ += cameraDirection.z * 0.1;
 		}
 		// 's'를 누르고 있을 때 뒤로 이동
 		if (SPressed) {
-			camera.position.x -= cameraDirection.x * 0.1;
-			camera.position.z -= cameraDirection.z * 0.1;
+			moveX -= cameraDirection.x * 0.1;
+			moveZ -= cameraDirection.z * 0.1;
 		}
+
+		if (
+			camera.position.x + moveX <= 4.73 &&
+			camera.position.x + moveX >= -6.26 &&
+			camera.position.z + moveZ <= 9.19 &&
+			camera.position.z + moveZ >= -7.69
+		  ) {
+			camera.position.x += moveX;
+			camera.position.z += moveZ;
+		  }
 
 	   renderer.render(scene,camera);
 	   requestAnimationFrame(animate);
